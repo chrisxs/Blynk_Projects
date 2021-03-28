8ce509d9162b6db5451dcde29913e56d9a8f4e87
滑稽哥 <chrisxs@qq.com>
2021-03-28 18:49:39 +0800

    fix

---
+++ D1_Mini__GY39_OTA_WM/src/main.cpp
@@ -0,0 +1,286 @@
+#define BLYNK_PRINT Serial
+
+#include <FS.h> //this needs to be first, or it all crashes and burns...
+#include <Arduino.h>
+
+/////WiFiManager/////
+#include <ESP8266WiFi.h> //https://github.com/esp8266/Arduino
+#include <ESP8266WebServer.h>
+#include <DNSServer.h>
+#include <WiFiManager.h> //https://github.com/tzapu/WiFiManager
+#include <ArduinoJson.h> //https://github.com/bblanchon/ArduinoJson
+
+/////OTA/////
+#include <ArduinoOTA.h>
+#include <WiFiUdp.h>
+
+////Blynk/////
+#include <BlynkSimpleEsp8266.h>
+
+#include <string>
+#include <stdlib.h>
+
+/////传感器/////
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <MAX44009.h>
+
+
+//用于WiFiManager界面中的变量服务器域名、端口、口令
+std::string blynk_server;
+std::string blynk_port;
+std::string blynk_token;
+
+BlynkTimer timer;
+
+//标记是否储存
+bool shouldSaveConfig = false;
+
+const int ResetButton = D5;
+int ResetButtonState = digitalRead(ResetButton);
+
+MAX44009 light;
+Adafruit_BME280 bme; // I2C
+#define SEALEVELPRESSURE_HPA (1020)
+
+void sendSensor()
+{
+    float t = bme.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+    float h = bme.readHumidity();
+    float p = bme.readPressure() / 100.0F;
+    float l = light.get_lux();
+
+    Blynk.virtualWrite(V0, h);
+    Blynk.virtualWrite(V1, t);
+    Blynk.virtualWrite(V2, p);
+    Blynk.virtualWrite(V3, l);
+}
+
+//回调通知我们需要保存配置
+void saveConfigCallback()
+{
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  pinMode(ResetButton, INPUT_PULLUP);
+  unsigned status;
+  bme.begin(0x76);
+  light.begin();
+
+  /////WiFiManager/////
+
+  //clean FS, for testing
+  //SPIFFS.format();
+
+  //read configuration from FS json
+  Serial.println("mounting FS...");
+
+  if (SPIFFS.begin())
+  {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json"))
+    {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile)
+      {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+
+#ifdef ARDUINOJSON_VERSION_MAJOR >= 6
+        DynamicJsonDocument json(1024);
+        auto deserializeError = deserializeJson(json, buf.get());
+        serializeJson(json, Serial);
+        if (!deserializeError)
+        {
+#else
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject &json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success())
+        {
+#endif
+          Serial.println("\nparsed json");
+          blynk_server = json["blynk_server"].as<const char *>();
+          blynk_port = json["blynk_port"].as<const char *>();
+          blynk_token = json["blynk_token"].as<const char *>();
+        }
+        else
+        {
+          Serial.println("failed to load json config");
+        }
+        configFile.close();
+      }
+    }
+  }
+  else
+  {
+    Serial.println("failed to mount FS");
+  }
+  //end read
+
+  WiFiManagerParameter custom_blynk_server("server", "blynk server", blynk_server.c_str(), 40);
+  WiFiManagerParameter custom_blynk_port("port", "blynk port", blynk_port.c_str(), 6);
+  WiFiManagerParameter custom_blynk_token("blynk", "blynk token", blynk_token.c_str(), 32);
+  WiFiManagerParameter custom_text("<p>点击SSID名称选择连接WiFi,并输入密码/服务器地址/设备口令</p>");
+
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+
+  //set config save notify callback
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+  //add all your parameters here
+  wifiManager.addParameter(&custom_blynk_server);
+  wifiManager.addParameter(&custom_blynk_port);
+  wifiManager.addParameter(&custom_blynk_token);
+  wifiManager.addParameter(&custom_text);
+
+  //reset settings - for testing
+  //wifiManager.resetSettings();
+
+  if (ResetButtonState == LOW)
+  {
+    Serial.println("Getting Reset ESP Wifi-Setting.......");
+    wifiManager.resetSettings();
+    delay(5000);
+
+    Serial.println("Formatting FS......");
+    SPIFFS.format();
+    ESP.restart();
+  }
+
+  if (!wifiManager.autoConnect("GY39-DEMO", ""))
+  {
+    Serial.println("failed to connect and hit timeout");
+    delay(3000);
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(5000);
+  }
+
+  //if you get here you have connected to the WiFi
+  Serial.println("connected.)");
+
+  //read updated parameters
+  blynk_server = custom_blynk_server.getValue();
+  blynk_port = custom_blynk_port.getValue();
+  blynk_token = custom_blynk_token.getValue();
+  Serial.println("The values in the file are: ");
+  Serial.println("\tblynk_server: " + String(custom_blynk_server.getValue()));
+  Serial.println("\tblynk_port : " + String(custom_blynk_port.getValue()));
+  Serial.println("\tblynk_token : " + String(custom_blynk_token.getValue()));
+
+  //save the custom parameters to FS
+  if (shouldSaveConfig)
+  {
+    Serial.println("saving config");
+#ifdef ARDUINOJSON_VERSION_MAJOR >= 6
+    DynamicJsonDocument json(1024);
+#else
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject &json = jsonBuffer.createObject();
+#endif
+    json["blynk_server"] = blynk_server.c_str();
+    json["blynk_port"] = blynk_port.c_str();
+    json["blynk_token"] = blynk_token.c_str();
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile)
+    {
+      Serial.println("failed to open config file for writing");
+    }
+
+#ifdef ARDUINOJSON_VERSION_MAJOR >= 6
+    serializeJson(json, Serial);
+    serializeJson(json, configFile);
+#else
+    json.printTo(Serial);
+    json.printTo(configFile);
+#endif
+    configFile.close();
+    //end save
+  }
+
+  Serial.println("local ip");
+  Serial.println(WiFi.localIP());
+  delay(500);
+
+  /////OTA/////
+  ArduinoOTA.setHostname("GY39_DEMO_OTA");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+    {
+      type = "sketch";
+    }
+    else
+    { // U_SPIFFS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+
+  //ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+  //  Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  //});
+
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR)
+    {
+      Serial.println("Auth Failed");
+    }
+    else if (error == OTA_BEGIN_ERROR)
+    {
+      Serial.println("Begin Failed");
+    }
+    else if (error == OTA_CONNECT_ERROR)
+    {
+      Serial.println("Connect Failed");
+    }
+    else if (error == OTA_RECEIVE_ERROR)
+    {
+      Serial.println("Receive Failed");
+    }
+    else if (error == OTA_END_ERROR)
+    {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  Blynk.config(blynk_token.c_str(), blynk_server.c_str(), std::atoi(blynk_port.c_str()));
+  timer.setInterval(1000L, sendSensor);
+}
+
+void loop()
+{
+  ArduinoOTA.handle();
+  Blynk.run();
+  timer.run();
+}
