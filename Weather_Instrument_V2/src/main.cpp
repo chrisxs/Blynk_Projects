1bfcf49b82be688a590373ea369fde60e3e71dfc
滑稽哥 <chrisxs@qq.com>
2023-03-25 23:33:46 +0800

    add

--- Weather_Instrument_V2/src/main.cpp
+++ Weather_Instrument_V2/src/main.cpp
@@ -28,7 +28,7 @@ char auth[] = "你的Blynk_Token";
 char ssid[] = "你的WiFi_SSID";
 char pass[] = "你的WiFi密码";
 char blynk_server[] = "你的Blynk服务器路径"; // Blynk服务器路径
-int blynk_port = 8080;               // Blynk端口号
+int blynk_port = 8080;                       // Blynk端口号
 
 BlynkTimer timer;
 
